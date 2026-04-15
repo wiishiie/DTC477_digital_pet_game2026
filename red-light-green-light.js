17aac3585f0b9b4714b64a34a42ca2366d0e4dbf
wiishiie <isabelle.sullivan@wsu.edu>
2026-04-14 18:37:47 -0700

    added more details to task list

--- red-light-green-light.js
+++ red-light-green-light.js
@@ -1,8 +1,9 @@
 //crystal
 // I created a simple movement function in the canvas.js file, you can use that to help make sure the game works. 
 // tasks:
+//create a pop up before the game starts explainging the game with a play button in it. Have a 3 second count down when the play button is hit to prepare the user.
 //create a square for now that changes between red and green that either sits at the top of the page, or in the top corner
-//create a ten second count down. If the user fails by: moving on red, or not completeing the game in time, makea  pop up that says the lost, with a button to 
+//create a 10 second count down. If the user fails by: moving on red, or not completeing the game in time, make a pop up that says they lost, with a button to 
 // wipe the canvas and "go back home". If they win the game, create a pop up that says they earned x amount of molney and x amount of exp. you can leave these 
 // blank for now, and should follow the format of <coins: {$earnedCurrency}>  so we can randomize the amount they earn each time
 // create a function that switches between red and green with varying time limits, such as creating a math.Random with a rang of 2-5 seconds. The red light should 
